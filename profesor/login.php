8daa4a726b0ba1f6629fe79a6199c20bafa6e160
AngelTorres96 <angeltorres7850@gmail.com>
2019-03-04 22:36:34 -0600

    cartpeta de profesores

---
+++ profesor/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Seguimiento de atributos</title>
+    <!--Fuentes de google-->
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One|PT+Sans+Narrow" rel="stylesheet">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/all.min.css">
+    <link rel="stylesheet" href="../css/main.css">
+
+
+</head>
+
+<body>
+<?php include '../templates/header.php'; ?>
+<div class="container">
+  <div id="row">
+  <div class="row">
+      <div class="col-md-12 titulo">
+          <h2>Inicia sesión como administrador</h2>
+      </div>
+  </div>
+
+  <div class="row" style="padding-left: 20em; padding-right: 20em">
+    <br>
+    <form class="col-md-12"  action="../function/login_bd.php" method="post">
+      <br><br>
+      <input type="text" class="form-control" name="txtusuario" value="" placeholder="Usuario"><br><br>
+      <input type="password" class="form-control" name="txtpass" value="" placeholder="Contraseña"><br><br>
+      <button type="submit" class="btn" name="login">Iniciar sesión</button>
+    </form>
+  </div>
+
+  <br>
+  <br>
+  <br>
+  <br>
+  </div>
+</div>
+</body>
+</html>
