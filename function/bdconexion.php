be33309cdfc7bc6ddd30443e78f7b8efcca66b08
DanielGR <wicho94k@outlook.com>
2019-04-15 16:49:31 -0500

    xx

--- function/bdconexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-    //Ingresar Servidor, usuario, contraseña, nombre de la bd
-    $conn = new mysqli('localhost','root','','atributos');
-    if($conn->connect_error) {
-        echo $error->$conn->connect_error;
-    }
-?>
--- function/get_paginas.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-  include_once 'bdconexion.php';
-  $sql_registe=$conn->query("SELECT COUNT(*) AS total_registro FROM materia");
-  $result_register = $sql_registe->fetch_assoc();
-  $total_registro = $result_register['total_registro'];
-  echo $total_registro;
- ?>
