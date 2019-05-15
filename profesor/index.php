195bddb161a2842763276da253a2bfb0dc1d6a81
DanielGR <wicho94k@outlook.com>
2019-05-14 19:56:13 -0500

    update

--- profesor/index.php
+++ profesor/index.php
@@ -600,7 +600,7 @@
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
                     <!--???????????????????????????????????????????????????????-->
-                    <button type="button" class="btn btn-primary" onclick="confirmModGrupo()">Aceptar</button>
+                    <button type="button" class="btn btn-primary" onclick="confirmUpdateGrupo()">Aceptar</button>
                   </div>
                 </div>
               </div>
